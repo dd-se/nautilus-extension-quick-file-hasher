99b3f6f89096a3aaf6a4652934d46c7b015d96ab
dd-se <76243082+dd-se@users.noreply.github.com>
2025-08-31 18:14:16 +0200

    Bump version to 2.0.5

--- quick-file-hasher-app.py
+++ quick-file-hasher-app.py
@@ -54,7 +54,7 @@ from gi.repository import Adw, Gdk, Gio, GLib, GObject, Gtk, Nautilus, Pango  # 
 
 APP_ID = "com.github.dd-se.quick-file-hasher"
 APP_NAME = "Quick File Hasher"
-APP_VERSION = "2.0.0"
+APP_VERSION = "2.0.5"
 
 DEFAULTS = {
     "algo": "sha256",
